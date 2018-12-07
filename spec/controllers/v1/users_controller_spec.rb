8904578d41520cd61297ea7ae380bb00de37ff42
Andrew Dymond <andy.dymond@gmail.com>
2018-12-07 15:48:12 -0700

    Add users controller spec

---
+++ spec/controllers/v1/users_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe V1::UsersController do
+  let(:valid_user_params) {
+    { email: "test@user.com",
+      password: "Password1!",
+      password_confirmation: "Password1!"
+    }
+  }
+
+  let(:missing_param) { [:email, :password, :password_confirmation] }
+
+  context "valid user registration credentials" do
+    before { post :create, params: { user: valid_user_params } }
+
+    it { expect(response).to have_http_status(201) }
+    it { expect(JSON.parse(response.body)).to eq({ "success" => User.last.id }) }
+  end
+
+  context "invalid user registration credentials" do
+    before { post :create, params: { user: valid_user_params.except(missing_param.sample) } }
+
+    it { expect(response).to have_http_status(409) }
+    it { expect(JSON.parse(response.body)).to eq({ "failure" => "user creation failed" })}
+  end
+end
