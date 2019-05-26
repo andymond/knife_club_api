ecf3d57aecb8d3c5ac4f32bb3882e3009d183fe9
Andrew Dymond <andy.dymond@gmail.com>
2019-05-26 11:50:09 -0600

    Add validations spec to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,18 @@ describe User do
     it { should have_many(:user_roles) }
     it { should have_many(:roles) }
   end
+
+  context "validations" do
+    it "validates password" do
+      user = described_class.new
+      user.password = "xy"
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 3 characters)")
+      should validate_presence_of(:password_confirmation)
+    end
+
+    it "validates email" do
+      should validate_presence_of(:email)
+    end
+  end
 end
