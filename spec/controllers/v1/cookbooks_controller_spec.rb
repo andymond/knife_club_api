5f6763221679a090b17a29fcd31c7d9f73a7547f
Andrew Dymond <andy.dymond@gmail.com>
2019-05-27 14:51:44 -0600

    Add cookbooks controller spec for show

--- spec/controllers/v1/cookbooks_controller_spec.rb
+++ spec/controllers/v1/cookbooks_controller_spec.rb
@@ -1,10 +1,11 @@
 require "rails_helper"
 
 describe V1::CookbooksController, type: :controller do
-  context "#create" do
-    let(:user) { create(:user, password: "coolpassword", password_confirmation: "coolpassword") }
-    let(:token) { ApiSessionManager.new(user.id).try_login("coolpassword")[:token] }
-    let(:auth_headers) { { "User" => user.id, "Authorization" => token } }
+  let(:user) { create(:user, password: "coolpassword", password_confirmation: "coolpassword") }
+  let(:token) { ApiSessionManager.new(user.id).try_login("coolpassword")[:token] }
+  let(:auth_headers) { { "User" => user.id, "Authorization" => token } }
+
+  describe "#create" do
     let(:create_request) { post :create, params: { name: "Test Cookbook" } }
     before { request.headers.merge(auth_headers) }
 
@@ -18,6 +19,36 @@ describe V1::CookbooksController, type: :controller do
       expect(json_response[:id]).to be_an(Integer)
       expect(json_response[:name]).to eq("Test Cookbook")
       expect(json_response[:public]).to eq(false)
+    end
+  end
+
+  let(:user_2) { create(:user, password: "coolpassword", password_confirmation: "coolpassword") }
+  let(:token_2) { ApiSessionManager.new(user.id).try_login("coolpassword")[:token] }
+  let(:auth_headers_2) { { "User" => user_2.id, "Authorization" => token_2 } }
+
+  describe "#show" do
+    before(:each) { request.headers.merge(auth_headers_2) }
+
+    context "cookbook is public" do
+      let(:public_cookbook) { user.cookbooks.create(name: "Indian Food", public: true) }
+      let(:create_public_cb_request) { get :show, params: { id: public_cookbook_id } }
+
+      it "allows user to view cookbook" do
+        create_public_cb_request
+
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "cookbook is private" do
+      let(:private_cookbook) { user.cookbooks.create(name: "Top Secret Sauces") }
+      let(:create_private_cb_request) { get :show, params: { id: private_cookbook_id } }
+
+      it "prevents user without permission from viewing cookbook" do
+        create_private_cb_request
+
+        expect(response).to have_http_status(404)
+      end
     end
   end
 end
