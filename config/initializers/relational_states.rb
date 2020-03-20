6af616321c927d72ec257d7e7e265bc32ca539e1
Andrew Dymond <andy.dymond@gmail.com>
2020-03-20 17:45:44 -0600

    Update read role to reader for semantic consistency

--- config/initializers/relational_states.rb
+++ config/initializers/relational_states.rb
@@ -1,2 +1,2 @@
 # single truth for state managed in postgres tables
-Rails.application.config.user_roles = %w(owner contributor read)
+Rails.application.config.user_roles = %w(owner contributor reader)
